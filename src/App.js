3c5c39c866ba0ccd0cd66e0e5cd213c6ceb2e9d2
Hooni <ksstpgns1@gmail.com>
2021-08-31 23:14:25 +0900

    20210830

--- src/App.js
+++ src/App.js
@@ -1,30 +1,50 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const FoodILike = [
   {
     id: 1,
-    name: "Kimchi"
+    name: "Kimchi",
+    image: "https://www.bgw.kr/wp-content/uploads/2019/12/%ED%8F%AC%EA%B8%B0%EA%B9%80%EC%B9%98-1200x1200.png",
+    rating: 1
   },
   {
     id: 2,
-    name: "Kimbap"
+    name: "Kimbap",
+    image: "https://www.bgw.kr/wp-content/uploads/2019/12/%ED%8F%AC%EA%B8%B0%EA%B9%80%EC%B9%98-1200x1200.png",
+    rating: 2
   },
   {
     id: 3,
-    name: "Doncasu"
+    name: "Doncasu",
+    image: "https://www.bgw.kr/wp-content/uploads/2019/12/%ED%8F%AC%EA%B8%B0%EA%B9%80%EC%B9%98-1200x1200.png",
+    rating: 3
   }
 ];
 
-function Food({ name, picture }) {
+function Food({ name, picture, rating }) {
   return (
     <div>
       <h1>I like {name}</h1>
+      <h4>{rating}/5</h4>
       <img src={picture} alt={name} />
-    </div>)
+    </div>
+  );
 }
 
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+};
+
 function rederFood(dish) {
-  return <Food key={dish.id} name={dish.name} picture={dish.picture} />
+  return <Food
+    key={dish.id}
+    name={dish.name}
+    picture={dish.image}
+    rating={dish.rating}
+  />
 }
 
 function App() {
